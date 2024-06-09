a8bb7844a59ae5c8542f8d16991725bf0749626c
“Psyc023” <lizard0623@gmail.com>
2024-06-08 23:42:16 -0600

    tailwind css added

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,15 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2">
+          Botón Primario
+        </button>
+        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+          Botón Secundario
+        </button>
         <a
           className="App-link"
           href="https://reactjs.org"
