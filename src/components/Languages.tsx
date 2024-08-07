6845beb0c7e0f427b4d4c7488b1052d11c1f1518
Felipe Canseco Lopez <lizard0623@gmail.com>
2024-08-07 15:11:23 -0600

    testing japanese kanjis

--- src/components/Languages.tsx
+++ src/components/Languages.tsx
@@ -16,7 +16,7 @@ const Languages: React.FC = () => {
       <a
         className="mx-4 bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4 rounded-full"
       >
-        Japanese
+        日本語
       </a>
     </div>
   );
