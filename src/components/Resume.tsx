7e18fe5f52c321c3dc4e007f45cde55f582c1bc0
Felipe Canseco Lopez <lizard0623@gmail.com>
2024-09-09 10:09:29 -0600

    test git config

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const Resume: React.FC = () => {
     return (
         <div style={{ backgroundColor: 'green', minHeight: '100vh', color: 'white' }}>
-          {/* Contenido de tu componente Experience */}
+          {/* Contenido de tu   componente   Experience */}
         </div>
       );
     };
